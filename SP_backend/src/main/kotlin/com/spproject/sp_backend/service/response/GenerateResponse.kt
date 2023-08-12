d92e243d19d7c3d14b0011d8ef23468c444e23e7
KyumKyum <mycheesepasta@gmail.com>
2023-08-12 21:12:01 +0900

    Feat: Add Failure Response

---
+++ SP_backend/src/main/kotlin/com/spproject/sp_backend/service/response/GenerateResponse.kt
@@ -0,0 +1,23 @@
+package com.spproject.sp_backend.service.response
+
+open class GenerateResponse {
+    protected fun setSuccessfulResponse(response: ServiceResponse) {
+        response.setVal(
+            ok = true,
+            code = "SUCCESS",
+            message = "Response Successful"
+        )
+    }
+
+    protected fun setFailureResponse(
+        response: ServiceResponse,
+        code: String,
+        message: String
+    ) {
+        response.setVal(
+            ok = false,
+            code,
+            message
+        )
+    }
+}
